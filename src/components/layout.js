f633370a83684ed89adfcc6f69a98e3075779b8d
Kishore Kumar G <kishorekumar080297@gmail.com>
2019-08-06 01:49:14 +0530

    Added Footer Component under reusable

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,3 +1,5 @@
+//Created by Kishore Kumar Govindaradjou on 8th Aug 2019
+
 /**
  * Layout component that queries for data
  * with Gatsby's useStaticQuery component
@@ -8,42 +10,17 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import Footer  from './reusable/Footer'
 
-import Header from "./header"
+import "./bootstrap.min.css"
 import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
+const Layout = ({children}) => (
+  <>
+  {children}
+  <Footer />
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
---
+++ src/components/reusable/Footer.js
@@ -0,0 +1,20 @@
+//Created by Kishore Kumar Govindaradjou on 8th Aug 2019
+import React from 'react'
+import { Z_BLOCK } from 'zlib';
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="footer-block">
+                <div className="contianer-fluid">
+                    <div className="row">
+                        <div className="col-12 pt-2 mx-auto text-center bg-dark text-light">
+                            <h6>FoneBayon &copy; 2019</h6>
+                            <h6>&lt;&nbsp;Developed by <a href="https://kishore-kumar.herokuapp.com"><b>Kishore Kumar Govindaradjou</b></a>&nbsp;/&gt;</h6>
+                        </div>
+                    </div>                 
+                </div>
+            </div>   
+        </footer>
+    )
+}
