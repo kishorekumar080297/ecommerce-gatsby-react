ade9ff754e688a955a72309d96f7efb27167a908
Kishore Kumar G <kishorekumar080297@gmail.com>
2019-08-14 00:47:59 +0530

    Added gatsby background plugin and GraphQL

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,17 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import HeroSection from "../components/reusable/HeroSection"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <HeroSection 
+    title="FoneBayOn"
+    subtile="The Smartphone Ocean"
+    heroClass="hero-background"
+    />
   </Layout>
-  // <h1>Hi people</h1>
-  // <p>Welcome to your new Gatsby site.</p>
-  // <p>Now go build something great.</p>
-  // <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-  // </div>
 )
 
 export default IndexPage
