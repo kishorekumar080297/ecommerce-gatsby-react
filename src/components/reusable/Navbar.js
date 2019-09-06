84171c1a71fae5aacfbb5fdd72a598757fb9f267
Kishore Kumar G <kishorekumar080297@gmail.com>
2019-09-07 01:32:43 +0530

    Getting started with contentful

--- src/components/reusable/Navbar.js
+++ src/components/reusable/Navbar.js
@@ -44,7 +44,7 @@ export default class Navbar extends Component {
 
     render() {
         return (
-            <nav className="navbar navbar-expand-sm bg-theme text-white">
+            <nav className="navbar fixed-top navbar-expand-sm bg-theme text-white">
                 <Link to="/" className="navbar-brand ml-5  text-white nav-text-brand">
                     <img src={logo} alt="FoneBayOn Logo" height= "50px" className="d-inline-block align-center"/>    
                     &nbsp;FoneBayOn
